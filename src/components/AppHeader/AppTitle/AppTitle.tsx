5e867bacfda709ef4a5d41a7cd6a20427569d8e8
carsnwd <carsnwd@gmail.com>
2025-02-09 13:40:02 -0500

    Update the responsiveness of the title

--- src/components/AppHeader/AppTitle/AppTitle.tsx
+++ src/components/AppHeader/AppTitle/AppTitle.tsx
@@ -1,11 +1,16 @@
 import { Title } from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks';
 import { IconMovie } from '@tabler/icons-react'
 
 export default function AppTitle() {
+    const isSmallScreen = useMediaQuery('(max-width: 768px)');
     return (
         <>
-            <Title order={1} style={{ display: "flex", alignItems: "center" }}>
-                <IconMovie size={52} style={{ marginRight: 10 }} />
+            <Title order={1} style={{
+                display: "flex", alignItems: "center",
+                fontSize: isSmallScreen ? '1.5rem' : '2.5rem',
+            }}>
+                <IconMovie size={isSmallScreen ? 32 : 55} style={{ marginRight: 10 }} />
                 Movie Watchlist
             </Title>
         </>
